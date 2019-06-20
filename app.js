12565133e52e36f5c6f6c7fd90c99cdfa7aeba8a
mendox0220 <mendox0220@gmail.com>
2019-06-21 08:40:24 +0900

    corrent update

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const http = require("http").createServer(app)
 const io = require("socket.io")(http)
 
+// var nicknames =[];
+
 app.use(express.static("public"))
 app.use(express.static("node_modules"))
 
@@ -11,6 +13,17 @@ app.get('/',(req, res)=>{
 })
 
 io.on('connection', socket =>{
+    // socket.on("new user", function(data, callback){
+    //     if (nicknames.indexOf(data) != -1){
+    //         callback(false);
+    //     }
+    //     else {
+    //         callback(true);
+    //         socket.nicknames = data;
+    //         nicknames.push(socket.nickname);
+    //         io.sockets.emit("usernames", nicknames);
+    //     }
+    // })
     socket.on("sendServer", data=> {
         io.sockets.emit("sendClient", data)
     })
